caf38c2e215e3831389d72d3e4b744fb2a0f16e6
RodriSanchez1 <rodri.sanchez1998@gmail.com>
2021-10-04 11:38:09 -0300

    first commit

---
+++ MSIapp/src/msiapp/MSIapp.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package msiapp;
+
+import Ventana.Carga_Producto;
+import Ventana.Ingresar_Producto;
+import Ventana.Listado_Productos;
+import Ventana.Modificar_Ubicacion_Producto;
+import Ventana.Ubicar_Producto;
+import Ventana.VentanaLogin;
+
+/**
+ *
+ * @author ginin
+ */
+public class MSIapp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+                new VentanaLogin().setVisible(true);
+                new Carga_Producto().setVisible(true);
+                new Ingresar_Producto().setVisible(true);
+                                new Listado_Productos().setVisible(true);
+                new Ubicar_Producto().setVisible(true);
+                new Modificar_Ubicacion_Producto().setVisible(true);
+
+    }
+
+    
+}
